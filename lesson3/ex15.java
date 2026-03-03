36aa43e263ff715500dfd4b117029d91ba7cb971
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:13:56 -0300

    feat(lesson3): Implement ex15

--- lesson3/ex15.java
+++ lesson3/ex15.java
@@ -3,3 +3,16 @@
  * base no seu raio (Área = PI*raio*raio, onde PI = 3.14159). 
  * Use variáveis reais.
  */
+
+import java.util.Scanner;
+
+public class ex15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o raio do círculo: ");
+        double radius = scanner.nextDouble();
+
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("A área do círculo é: " + area);
+    }
+}
