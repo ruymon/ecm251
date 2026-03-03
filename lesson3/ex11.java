9d23f935d7b6a70bf79905c791f712d1d005cced
ruymon <ruystfrancis@gmail.com>
2026-03-03 09:50:53 -0300

    feat: add algorithm to shuffle messages by combining halves of three input phrases

---
+++ lesson3/ex11.java
@@ -0,0 +1,12 @@
+/**
+ * Crie um algoritmo que embaralhe mensagens fazendo:
+ * - leia três frases, separe cada uma delas ao meio.
+ * Então junte nesta ordem: primeira metade da segunda,
+ * - segunda metade da terceira,
+ * - segunda metade da segunda,
+ * - primeira metade da primeira,
+ * - primeira metade da terceira,
+ * - segunda metade da primeira. 
+ * - Concatene então as três frases originais e imprima o resultado. 
+ * - Na linha de baixo, escreva a frase embaralhada e compare o resultado
+ */
