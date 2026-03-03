087cd2bdddaa4ddd209c0f7838ab3d063494ce5e
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:13:03 -0300

    feat(lesson3): Implement ex4

--- lesson3/ex4.java
+++ lesson3/ex4.java
@@ -3,3 +3,20 @@
  * digitadas por meio do console, e apresente a soma do
  * comprimento destas Strings.
  */
+
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a primeira string: ");
+        String firstString = scanner.nextLine();
+        System.out.println("Digite a segunda string: ");
+        String secondString = scanner.nextLine();
+        System.out.println("Digite a terceira string: ");
+        String thirdString = scanner.nextLine();
+
+        int length = firstString.length() + secondString.length() + thirdString.length();
+        System.out.println("A soma do comprimento das strings é: " + length);
+    }
+}
