f52d5ff260c785db086141376dd3b4181adfa2b9
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:13:52 -0300

    feat(lesson3): Implement ex14

--- lesson3/ex14.java
+++ lesson3/ex14.java
@@ -4,3 +4,20 @@
  * 
  * x = 2 * ( ( a – c ) / 8 ) – b * 5.
  */
+
+import java.util.Scanner;
+
+public class ex14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor de a: ");
+        double a = scanner.nextDouble();
+        System.out.println("Digite o valor de b: ");
+        double b = scanner.nextDouble();
+        System.out.println("Digite o valor de c: ");
+        double c = scanner.nextDouble();
+
+        double x = 2 * ((a - c) / 8) - b * 5;
+        System.out.println("O valor de x é: " + x);
+    }
+}
