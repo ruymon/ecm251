02f750337d257e5e08e671dc04b71529179bea8b
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:14:13 -0300

    feat(lesson3): Implement ex19

--- lesson3/ex19.java
+++ lesson3/ex19.java
@@ -2,3 +2,18 @@
  * Criar um algoritmo que calcule e imprime a área de um
  * losango.
  */
+
+import java.util.Scanner;
+
+public class ex19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a diagonal maior do losango: ");
+        double majorDiagonal = scanner.nextDouble();
+        System.out.println("Digite a diagonal menor do losango: ");
+        double minorDiagonal = scanner.nextDouble();
+
+        double area = (majorDiagonal * minorDiagonal) / 2;
+        System.out.println("A área do losango é: " + area);
+    }
+}
