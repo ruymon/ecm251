cd4bfa97cff9ffb8e51cdeee25cb9c4463969505
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:13:23 -0300

    feat(lesson3): Implement ex9

--- lesson3/ex9.java
+++ lesson3/ex9.java
@@ -1,3 +1,16 @@
 /**
  * Entrar com um número e imprimir o seu logaritmo na base 10
  */
+
+import java.util.Scanner;
+
+public class ex9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        double number = scanner.nextDouble();
+
+        double logarithm = Math.log10(number);
+        System.out.println("O logaritmo do número é: " + logarithm);
+    }
+}
