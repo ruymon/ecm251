f999235a64c2021ed20c61f7a97bd693efbba695
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:13:31 -0300

    feat(lesson3): Implement ex10

--- lesson3/ex10.java
+++ lesson3/ex10.java
@@ -3,3 +3,19 @@
  * logaritmo desse número. Após isto, calcular tal logaritmo e
  * imprimir o resultado.
  */
+
+import java.util.Scanner;
+
+public class ex10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        double number = scanner.nextDouble();
+        System.out.println("Digite a base: ");
+        int base = scanner.nextInt();
+
+        double logarithm = Math.log(number) / Math.log(base);
+        
+        System.out.println("O logaritmo do número é: " + logarithm);
+    }
+}
