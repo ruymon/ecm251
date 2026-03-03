51062a6a93f77c2ab6bb31391c2639e0f31f8d43
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:14:00 -0300

    feat(lesson3): Implement ex16

--- lesson3/ex16.java
+++ lesson3/ex16.java
@@ -2,3 +2,23 @@
  * Ler dois números inteiros e imprimir dividendo, divisor,
  * quociente e resto.
  */
+
+import java.util.Scanner;
+
+public class ex16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número inteiro: ");
+        int dividend = scanner.nextInt();
+        System.out.println("Digite o segundo número inteiro: ");
+        int divisor = scanner.nextInt();
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Dividendo: " + dividend);
+        System.out.println("Divisor: " + divisor);
+        System.out.println("Quociente: " + quotient);
+        System.out.println("Resto: " + remainder);
+    }
+}
