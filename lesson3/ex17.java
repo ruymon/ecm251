c1d146fbac3a1f95d46263410aadf8dced74b4fe
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:14:04 -0300

    feat(lesson3): Implement ex17

--- lesson3/ex17.java
+++ lesson3/ex17.java
@@ -2,3 +2,20 @@
  * Entrar com um número e imprimir o número, seu quadrado e
  * sua raiz quadrada.
  */
+
+import java.util.Scanner;
+
+public class ex17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        double number = scanner.nextDouble();
+
+        double square = Math.pow(number, 2);
+        double squareRoot = Math.sqrt(number);
+
+        System.out.println("Número: " + number);
+        System.out.println("Quadrado: " + square);
+        System.out.println("Raiz quadrada: " + squareRoot);
+    }
+}
