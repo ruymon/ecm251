9d2a9ec9d8e1e1e3ff5436201bd2b236737102ff
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:13:13 -0300

    feat(lesson3): Implement ex7

--- lesson3/ex7.java
+++ lesson3/ex7.java
@@ -2,3 +2,17 @@
  * Entrar com um único número inteiro, com 5 dígitos, e
  * imprimir o algarismo correspondente à casa da dezena.
  */
+
+import java.util.Scanner;
+
+public class ex7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número inteiro com 5 dígitos: ");
+        int number = scanner.nextInt();
+
+        int tensDigit = number % 100 / 10;
+    
+        System.out.println("O algarismo correspondente à casa da dezena é: " + tensDigit);
+    }
+}
