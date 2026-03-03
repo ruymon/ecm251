e204d69c6bf958d8e49d7dac7fa5d25bead8336b
ruymon <ruystfrancis@gmail.com>
2026-03-03 09:50:28 -0300

    feat: add algorithm to calculate debt value with compound interest

---
+++ lesson3/ex6.java
@@ -0,0 +1,14 @@
+/**
+ * Crie um algoritmo que calcule o valor de uma dívida,
+ * submetida à juros compostos:
+ * 
+ * ValorFinal = ValorInicial * (1 + J / 100) ^ N
+ * 
+ * E seja capaz de responder, se você deve para o cartão de
+ * crédito R$ 100,00, a uma taxa de juros de 10%, quanto deverá
+ * ser pago depois de 8 meses.
+ * 
+ * Onde:
+ * - J representa os juros (em %);
+ * - N representa o número de meses;
+ */
