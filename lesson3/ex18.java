f4d36ff1fe17f7867b4fc484b41d7b023e99c4fe
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:14:08 -0300

    feat(lesson3): Implement ex18

--- lesson3/ex18.java
+++ lesson3/ex18.java
@@ -2,3 +2,18 @@
  * Criar um algoritmo que calcule e imprime a área de um
  * triângulo.
  */
+
+import java.util.Scanner;
+
+public class ex18 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a base do triângulo: ");
+        double base = scanner.nextDouble();
+        System.out.println("Digite a altura do triângulo: ");
+        double height = scanner.nextDouble();
+
+        double area = (base * height) / 2;
+        System.out.println("A área do triângulo é: " + area);
+    }
+}
