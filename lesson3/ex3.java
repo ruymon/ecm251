de746cabd1a5b995e657ac8c6c41df1698121698
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:12:58 -0300

    feat(lesson3): Implement ex3

--- lesson3/ex3.java
+++ lesson3/ex3.java
@@ -3,3 +3,17 @@
  * quadrado usando a classe Math, onde a base e o expoente
  * são números reais.
  */
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número inteiro: ");
+        
+        int number = scanner.nextInt();
+        double squared = Math.pow(number, 2);
+
+        System.out.println("O quadrado do número é: " + squared);
+    }
+}
