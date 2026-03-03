72daa23cb362c74796c8627d50a8e60643b4641f
ruymon <ruystfrancis@gmail.com>
2026-03-03 10:14:17 -0300

    feat(lesson3): Implement ex20

--- lesson3/ex20.java
+++ lesson3/ex20.java
@@ -3,3 +3,24 @@
  * triângulo, digitados os outros dois lados e o ângulo entre eles
  * esses dois lados.
  */
+
+import java.util.Scanner;
+
+public class ex20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro cateto do triângulo: ");
+        double firstCathetus = scanner.nextDouble();
+        System.out.println("Digite o segundo cateto do triângulo: ");
+        double secondCathetus = scanner.nextDouble();
+        System.out.println("Digite o ângulo entre os dois catetos: ");
+        double angle = scanner.nextDouble();
+
+        double angleInRadians = Math.toRadians(angle);
+
+        // Law of cosines
+        double thirdCathetus = Math.sqrt(Math.pow(firstCathetus, 2) + Math.pow(secondCathetus, 2) - 2 * firstCathetus * secondCathetus * Math.cos(angleInRadians));
+
+        System.out.println("O terceiro lado do triângulo é: " + thirdCathetus);
+    }
+}
